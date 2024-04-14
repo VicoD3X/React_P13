96fa2b699fe7e14c4619b9e16864921464da1683
Victor A <vicaubry@icloud.com>
2024-04-14 19:15:23 +0200

    FINAL - pre-soutenance V3
    - création et mise en route du composant de gestion de l'erreur 404 via "404.jsx" pour une gestion cohérente des erreurs de route non trouvées.
    - mise en place du composant 404.jsx via app.jsx afin de gerer la capture de toutes les routes inexistante.
    - finalisation du projet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
 import SignIn from './pages/sign-in';
 import UserIn from './pages/user-in'
+import Page404 from './components/404'
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/home/" element={<Home />} />
           <Route path="/home/sign-in" element={<SignIn />} />
           <Route path="/user/" element={<UserIn />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
     </>
---
+++ src/components/404.jsx
@@ -0,0 +1,13 @@
+// 404.jsx
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>404 - Page Not Found</h1>
+            <p>Désolé, la page que vous cherchez n'existe pas.</p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
